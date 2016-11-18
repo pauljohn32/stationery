5010dce2fd562f250a07581eb59935b666176d5e
Paul E. Johnson <pauljohn@ku.edu>
2016-11-18 17:29:24 -0600

    Calls the R function rnw2pdf from the shell. Needs some work to fancy up handling of empty command line.

--- package/crmda/inst/extdata/rnw2pdf-report-knit/rnw2pdf.sh
+++ package/crmda/inst/extdata/rnw2pdf-report-knit/rnw2pdf.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
 
-## Paul Johnson <pauljohn@ku.edu>
+fn=$1
 
-if [ $1 == "" ]
-then
-	filename="report-template.lyx"
-else
-    filename=$(basename "$1")
-fi
-extension="${filename##*.}"
-base="${filename%.*}"
-basen="${base}n.Rnw"
+Rscript -e "library(crmda); rnw2pdf(\"$fn\", engine = \"knitr\")"
 
-if [ $extension == ".lyx" ]
-   then
-	   lyx -e knitr "${base}.lyx"
-fi
-##R CMD knit "${base}.Rnw"
-Rscript -e "library(knitr); knit('${base}.Rnw')"
-texi2pdf "${base}.tex"
-
-##cp "${base}.Rnw" "${basen}"
-##perl -pi.bak -e 's/split=T/split=F/; s/prompt="\ *"/prompt="> "/'  $basen
-
-Rscript -e "library(knitr); knit('${base}.Rnw', tangle = TRUE)"
-
-mv "${base}n.R" "${base}.R"
-
+## TODO: come back and add if logic to process all Rmd
+## or lyx files
