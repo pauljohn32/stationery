3d00a4e0601b5d9d34f48e425e090489da93877c
Paul E. Johnson <pauljohn@ku.edu>
2016-11-28 23:25:33 -0600

    rnw2pdf.sh: knits from command line

--- package/crmda/inst/extdata/rnw2pdf-report-knit/rnw2pdf.sh
+++ package/crmda/inst/extdata/rnw2pdf-report-knit/rnw2pdf.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-fn=$1
+## Paul Johnson
+## 2016-11-28
 
-Rscript -e "library(crmda); rnw2pdf(\"$fn\", engine = \"knitr\")"
+## This knits files from knitr-syle "noweb" markup. See "engine" below.
 
-## TODO: come back and add if logic to process all Rmd
-## or lyx files
+## On command line, run 
+
+## 1 "./rnw2pdf.sh your_file.Rnw" to process "your_file.Rnw".
+##   Also works with "your_file.lyx" 
+## 2 "./rnw2pdf.sh *.Rnw" to process all Rnw files (or *.lyx for
+## all lyx files.
+
+flz=$@
+
+for fn in $flz
+  do 
+     echo $fn
+     Rscript -e "library(crmda); rnw2pdf(\"$fn\", engine = \"knitr\")"
+  done
+
+exit 0
+
