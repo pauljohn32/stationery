290d6ccbc45a4e103d4c9680ed065441135822a4
Paul E. Johnson <pauljohn@ku.edu>
2017-10-17 09:02:24 -0500

    cli-5 snapshot 20171017 AM

--- cli/cli-5.sh
+++ cli/cli-5.sh
@@ -1,28 +1,46 @@
 #/usr/bin/env bash
 
-## https://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options
-
-## This works, allegedly because of a flaw in POSIX argument processing.
-## 
-
-## Inserting die and check for value from mwiki.wooledge.org/BashFAQ/035
+## Paul Johnson
+## 20171016
 ##
 
-# Deal with arguments formatted as  --opt1=value1 by string splitting
-# Call whole thing OPTARG, then 
+## I'm grafting together 2 approaches to command line arguments.
+
+## 1. make getopts tolerate long arguments by declaring "-"
+##  then using manual "case" parsing on the
+## matches.
+## In
+## https://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options
+## there is an example, showing long arguments by using "-" as
+## the first character of a value being parsed.
+## Look for answer by @Arvid Requate
+
+## A shortcoming of that approach is that it does not protect against a user
+## error that omits the argument. So user error like "./cli-5.sh --loglevel --toc=TRUE"
+## causes an error, it thinks "--toc" is the value for loglevel.
+
+## 2. Manual argument checking.
+## Example in the Bash FAQ shows how to check arguments manually, to see
+## if no value is provided and die if that happens.
+## http://mwiki.wooledge.org/BashFAQ/035
+##
+
+## Why does that work? "-loglevel=5" becomes "loglevel=5" and then we split that
+## string at the equal sign, and treat fist piece as option name and
+## second piece as value.
+# In code below, see how I call whole thing OPTARG, then 
 # opt1=${OPTARG%=*} and value1=${OPTARG#*=}
 # See: https://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
 # ${var#*SubStr}  # will drop begin of string upto first occur of `SubStr`
-# ${var##*SubStr} # will drop begin of string upto last occur of `SubStr`
 # ${var%SubStr*}  # will drop part of string from last occur of `SubStr` to the end
-# ${var%%SubStr*} # will drop part of string from first occur of `SubStr` to the end
+# See also: http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 
-# More on Parameter Expansion
-# http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
-
-# In the "using-getopts" page, I find this usage that retrieves a value after a space
+# What I don't understand yet: 
+# In the "using-getopts" example, I find this usage that retrieves a value after a space
 # but i do not understand it.
 # val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+# this works, but I don't understand it!
+
 
 die() {
 	printf '%s\n' "$1" >&2
@@ -36,16 +54,23 @@ printparse(){
 loglevel=0
 toc="TRUE"
 
-optspec=":lthv-:"
+optspec=":l:t:hv-:"
 while getopts "$optspec" OPTCHAR; do
+	echo "OPTARG:  ${OPTARG[*]}"
+	echo "OPTIND:  ${OPTIND[*]}"
     case "${OPTCHAR}" in
         -)
             case "${OPTARG}" in
 				loglevel)
-					## retrieve next parameter , if exists
-					echo "OPTIND is {$OPTIND} {!OPTIND} is ${!OPTIND}"
-					if [ "${!OPTIND}" ]; then
-						val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+					opt=${OPTARG}
+					val="${!OPTIND}"
+					## check value exists in ${!OPTIND}
+					echo "OPTIND is {$OPTIND} {!OPTIND} is _${!OPTIND}_"
+					if [[ "$val" == -* ]]; then
+						die "ERROR: $opt value must be supplied"
+					fi
+					if [ -n "${!OPTIND}" ]; then
+							## OPTIND=$(( $OPTIND + 1 )) # CAUTION! no effect?
 						printparse ${OPTARG} " " ${val}
 						loglevel="${val}"
 						shift
@@ -54,26 +79,28 @@ while getopts "$optspec" OPTCHAR; do
 				    fi
 				    ;;
                 loglevel=?*)
-					opt=${OPTARG%=}
+					opt=${OPTARG%=*}
+					val=${OPTARG#*=}
 					if [ "${OPTARG#*=}" ]; then
-						val=${OPTARG#*=}
 						printparse ${opt} "=" ${val}
 						loglevel="${val}"
-						shift
+						## shift CAUTION don't shift this, fails othewise
 					else
 						die "ERROR: $opt value must be supplied"
 					fi
                     ;;
+				loglevel=)
+					opt=${OPTARG}
+			     	die "ERROR: $opt value must be supplied"
+				;;
+				
 				toc)
+					opt=${OPTARG}
 					echo "OPTIND is {$OPTIND} {!OPTIND} is ${!OPTIND}"
-					if [ "$1" ]; then
-					    opt=${OPTARG}
-						val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-						printparse ${OPTARG} " " ${val}
-						## Kill if value begins with dash
-						if [[ "$val" == -* ]]; then
-						   die "ERROR: $opt value must be supplied"
-						fi
+					val="${!OPTIND}"
+					if [ -n "$val" ]; then
+						## OPTIND=$(( $OPTIND + 1 )) #??
+						printparse ${opt} " " ${val}
 						toc="${val}"
 						shift
 					else
@@ -81,14 +108,20 @@ while getopts "$optspec" OPTCHAR; do
 				    fi
 				    ;;
 				toc=?*)
-                    opt=${OPTARG%=}
+                    opt=${OPTARG%=*}
+					val=${OPTARG#*=}
 					if [ "${OPTARG#*=}" ]; then
-						toc=${OPTARG#*=}
+						toc=${val}
 						printparse $opt " -> " $toc
+						##shift ## NO! dont shift this
 					else
                 		die "ERROR: value for $opt undefined"
 					fi
                     ;;
+				toc=)
+					opt=${OPTARG}
+			     	die "ERROR: $opt value must be supplied"
+				;;
 				help)
 					echo "usage: $0 [-v] [--loglevel[=]<value>] [--toc[=]<TRUE,FALSE>]" >&2
 					exit 2
@@ -104,6 +137,13 @@ while getopts "$optspec" OPTCHAR; do
             echo "usage: $0 [-v] [--loglevel[=]<value>] [--toc[=]<TRUE,FALSE>]" >&2
             exit 2
             ;;
+		l)
+			loglevel=${OPTARG}
+			;;
+		t)
+			toc=${OPTARG}
+			;;
+		
         v)
             echo "Parsing option: '-${OPTCHAR}'" >&2
             ;;
@@ -121,6 +161,16 @@ done
 ## ./cli-5.sh --loglevel=8
 ## ./cli-5.sh -l9
 
-echo "After Parsing values"
+## ./cli-5.sh  --toc FALSE --loglevel=77
+## ./cli-5.sh  --toc=FALSE --loglevel=77
+## ./cli-5.sh  --toc --loglevel=77
+
+## ./cli-5.sh   -l99 -t yyy
+## ./cli-5.sh   -l 99 -t yyy
+
+
+echo "
+After Parsing values
+"
 echo "loglevel  $loglevel" 
 echo "toc  $toc"
