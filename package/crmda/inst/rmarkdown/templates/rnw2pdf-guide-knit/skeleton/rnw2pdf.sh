851602fa4b098179f83d9ba1c6d6ed43b4ce4cf3
Paul E. Johnson <pauljohn@ku.edu>
2018-02-12 13:41:27 -0600

    rnw2pdf-guide-knit: rnw2pdf.sh now new command line argument style

--- package/crmda/inst/rmarkdown/templates/rnw2pdf-guide-knit/skeleton/rnw2pdf.sh
+++ package/crmda/inst/rmarkdown/templates/rnw2pdf-guide-knit/skeleton/rnw2pdf.sh
@@ -1,71 +1,171 @@
 #!/bin/bash
 
 ## Paul Johnson
-## 2017-08-08
+## 2018-02-10
 
+scriptname=`basename $0 .sh`
 
-## all Rnw files in working directory are compiled
-compileall() {
-	for fn in *.Rmd
-	do
-		Rscript -e "library(crmda); rnw2pdf(\"$fn\", $defaults)"
-	done
-}
-
-## Usage instruction, will compile all if user hits enter.
-usage() {
-  echo -e "\nUsage: $0 [filename.Rnw] or filename.lyx"
-  echo "No name is specified, so these Rmd files will be compiled:"
-  echo -e "\n" $(ls -1 *.Rnw) "\n"
-  echo -e "Hit Enter to process all Rmd files, or \"q\" to quit"
-  read -p "> " input
-  if [[ $input == "q" ]]; then
-   	  exit 1
-  else
-  echo "Compiling them all"
-  compileall
-  fi
-}
-
-pwd=`pwd`
-
-## The defaults string is R expression, inserted after
-## the file name argument in rmd2html.
-
-defaults="toc=TRUE, output_dir=\"$pwd\", type = \"guide\""
-## specify the template explicity
-## defaults="toc=TRUE, output_dir=\"$pwd\", template = \"theme/guide-boilerplate.html\""
-## Unless you specify otherwise, this will use the kutils.css file.
-
-## Retrieve the number of arguments
-nargs=$#
-
-## If no arguments, or if argument is "--help"
-if [[ $nargs -lt 1 ]]; then
-	usage
-elif [ "$1" == "--help" ]; then
-	usage
-fi
-
-## Process command line file names. Error on files that are not
-## suffixed with Rmd (ignoring case)
-for filename in "$@"; do
+## Receive one filename, then build it.
+compileOne(){
+	filename=$1
 	if [[ -e "$filename" ]]; then
 		fn=$(basename "$filename")
 		exten="${fn##*.}"
+		echo -e "extension is $exten"
 		## check extension, ignore case, allows "rmd" or "RMD"
 		shopt -s nocasematch
-		if [[ "$exten" == "Rnw" ]] || [[ "$exten" == "lyx" ]] ; then
+		if [[ ("$exten" -eq "Rnw") || ( "$exten" -eq "lyx") ]]; then
 			echo -e "compile $filename"
-			Rscript -e "library(crmda); rnw2pdf(\"$filename\", $defaults)"
+			Rscript -e "library(crmda); $scriptname(\"$filename\", $parmstring)"
 		else
 			echo -e "Error: $filename. Extension should be \"Rnw\" or \"lyx\""
 		fi
 	else
  		echo -e "$filename not found"
 	fi
+}
+
+## Scan directory for [Rmd,rmd] files and try to compile them
+compileall() {
+   	echo -e "\n""If no filename is specified, these Rmd files will be compiled:"
+	echo -e "\n" $(ls -1 *.Rmd) "\n"
+	echo -e "Hit Enter to continue, or \"q\" to quit"
+	read -p "> " input
+	if [[ $input == "q" ]]; then
+   		exit 1
+	else
+		echo "Compiling them all"
+		for fn in *.Rmd
+		do
+			compileOne "$fn"
+		done
+	fi
+}
+
+
+die() {
+	printf '%s\n' "$1" >&2
+	exit 1
+}
+
+## Only prints if DEBUG is set
+showme(){
+	if [ ${DEBUG} -gt 0 ]; then
+		printf 'DEBUG: %s\n' "$1" >&2;
+	fi
+}
+
+## Prints key=value pairs, one per line
+printarr() {
+	declare -n __p="$1"
+	for k in "${!__p[@]}"
+	do printf "     %s=%s\n" "$k" "${__p[$k]}"
+	done
+} 
+
+## builds $parmstring by concatenating key=value pairs
+catarr() {
+	declare -n __p="$1"
+	for k in "${!__p[@]}"
+	do parmstring+=", $k=${__p[$k]}"
+	done
+} 
+
+
+
+## Usage instruction.  
+usage() {
+	echo -e "\nUsage: $0 --arg=value [filename.Rmd]".
+	echo -e "Note: Because this document uses a template, the yaml header"
+	echo -e "format settings are ignored. Instead, specify arguments for this script."
+	echo "Current arguments:"
+	printarr parms
+    echo -e "\nThis script reformats and sends request to R as:\n"
+	echo -e "library(crmda); $scriptname(\"filename.Rmd\""$parmstring")\n"
+    echo "Add argument -v for VERBOSE output."
+	echo "Any arguments described in documentation for $scriptname R function are allowed."
+}
+
+## VERBOSE is flag users can turn on to get more detailed output
+VERBOSE=0
+## DEBUG if for author of this script, for fixing arg parsing. Not for users
+DEBUG=0
+pwd=`pwd`
+declare -A parms
+parms[engine]=\"knitr\"
+parms[tangle]=TRUE
+parms[verbose]=FALSE
+
+
+optspec=":vh-:"
+while getopts "$optspec" OPTCHAR; do
+    case "${OPTCHAR}" in
+        -)
+			showme "OPTARG:  ${OPTARG[*]}"
+			showme "OPTIND:  ${OPTIND[*]}"
+			showme "OPTCHAR: ${OPTCHAR}"
+			if [[ ${OPTARG}  == *"="* ]]; then
+				showme "There is an equal sign in ${OPTARG}"
+				opt=${OPTARG%=*}
+				val=${OPTARG#*=}
+				showme "--${opt} = \"${val}\""
+			   	if [[ -z $val ]]; then
+					die "ERROR: $opt is empty."
+				fi
+				parms[${opt}]=${val}
+			elif [[ ${OPTARG} == "help" ]]; then
+				## Finds "--help" Because no argument after help, then answer here
+				usage
+				exit
+			fi
+			;;
+	    h)
+            usage
+	    	exit 2
+		 	;;
+        v)
+			## if -v flag is present
+			echo -e "\n\nverbose flag\n"
+            VERBOSE=1
+			parms["quiet"]="FALSE"
+			## must print default parms here, before more parsing
+			echo "Parameters are:" >&2
+			printarr parms
+			;;
+        *)
+             if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+                 die "Undefined argument: '-${OPTARG}'"
+             fi
+             ;;
+	esac
 done
-exit 0
+
+parmstring=""
+catarr parms
+## echo "parmstring: $parmstring"
+
+## No shifts inside there, so must throw away arguments
+## that were processed
+shift "$((OPTIND-1))"
+
+showme "After getopts, N of args left $#"
+showme "Args:  $@"
+
+if [ ${VERBOSE} -gt 0 ]; then
+	echo -e "After parsing command line, the parameters are:"
+	printarr parms
+fi
+
+## Retrieve the number of arguments that are left
+nargs=$#
+## If no arguments for file names, print usage, build all. 
+if [[ $nargs -lt 1 ]]; then
+	usage
+	compileall
+fi
 
 
+for filename in "$@"; do
+ 	compileOne $filename
+done
 
