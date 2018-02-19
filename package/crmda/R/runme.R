479179fe2cd928b52d7070967052d87a76af0882
Paul E. Johnson <pauljohn@ku.edu>
2018-02-19 15:40:22 -0600

    runme: rnw2pdf had tangle as argument R/runme.R rnw2pdf now conditionalized for Windows, clean argument

--- package/crmda/R/runme.R
+++ package/crmda/R/runme.R
@@ -11,6 +11,8 @@
 ##' @param wd A working directory name of the Rmd file. If not
 ##'     specified, then the current working directory from R will be
 ##'     used.
+##' @param purl Default TRUE
+##' @param tangle Default TRUE, synonym for purl
 ##' @param verbose The opposite of render(quiet = TRUE). Shows compile
 ##'     commentary and pandoc command. Can be informative!
 ##' @param template An html template file, defaults as guide-boilerplate.html in this packge.
@@ -50,10 +52,11 @@
 ##' of2 <- rmd2html(fp, toc = FALSE, output_dir = getwd())
 ##' if(interactive()) browseURL(of2[1])
 ##' setwd(wd.orig)
-rmd2html <- function(fn = NULL, wd = NULL, verbose = FALSE,
+rmd2html <- function(fn = NULL, wd = NULL, verbose = FALSE, purl = TRUE, tangle = purl, 
                      template = system.file("theme", "guide-boilerplate.html", package = "crmda"),
-                     css = system.file("theme", "kutils.css", package = "crmda"),
+                     css = system.file("theme", "kutils.css", package = "crmda"), tangle = TRUE, 
                      ...) {
+    if (!missing(tangle) && is.logical(tangle)) purl <- tangle
     if (!is.null(wd)){
         wd.orig <- getwd()
         setwd(wd)
@@ -101,7 +104,7 @@ rmd2html <- function(fn = NULL, wd = NULL, verbose = FALSE,
         render_args <- list(input = x, output_format = htmldoc, quiet = !verbose,
                             envir = globalenv())
         render_argz <- modifyList(render_args, dots_for_render)
-        knitr::purl(fn)
+        if(purl) knitr::purl(fn)
         if(verbose) {print(paste("dots_for_render"));  lapply(dots_for_render, print)}
         do.call(rmarkdown::render, render_argz)
     })
@@ -152,6 +155,8 @@ crmda_html_document <- function(template = "custom_template", ...) {
 ##' @param verbose The opposite of render(quiet = TRUE). Shows compile
 ##'     commentary and pandoc command. Can be informative!
 ##' @param type either (default) "report" or "guide"
+##' @param purl Default TRUE
+##' @param tangle Default TRUE, synonym for purl
 ##' @param template An LaTeX template file, defaults for type = "report" as
 ##'     report-boilerplate.tex, or, if type = "guide" then
 ##'     guide-boilerplate.tex in this package.
@@ -189,9 +194,12 @@ crmda_html_document <- function(template = "custom_template", ...) {
 ##' if(interactive()) browseURL(of2[1])
 ##' setwd(wd.orig)
 rmd2pdf <- function(fn = NULL, wd = NULL, verbose = FALSE, type = "report",
-                    template = system.file("theme", paste0(type, "-boilerplate.tex"), package = "crmda"),
+                    purl = TRUE, tangle = purl,
+                    template = system.file("theme", paste0(type, "-boilerplate.tex"),
+                                           package = "crmda"),
                     package = "crmda",
                     ...) {
+    if(!missing(tangle) && is.logical(tangle)) purl <- tangle
     if (!is.null(wd)){
         wd.orig <- getwd()
         setwd(wd)
@@ -232,7 +240,7 @@ rmd2pdf <- function(fn = NULL, wd = NULL, verbose = FALSE, type = "report",
         render_args <- list(input = x, output_format = pdfdoc, quiet = !verbose,
                             envir = globalenv())
         render_argz <- modifyList(render_args, dots_for_render)
-        knitr::purl(fn)
+        if (purl) knitr::purl(fn)
         if(verbose) {print(paste("dots_for_render"));  lapply(dots_for_render, print)}
         do.call(rmarkdown::render, render_argz)
     })
@@ -257,7 +265,13 @@ rmd2pdf <- function(fn = NULL, wd = NULL, verbose = FALSE, type = "report",
 ##' @param wd Directory in which the file to be converted
 ##'     exists. Leave NULL default if is in current working directory.
 ##' @param engine "knitr" or "Sweave"
-##' @param verbose if FALSE, the knitr quiet is set as TRUE
+##' @param purl Default TRUE. Synonym of tangle: extract R code chunks
+##' @param tangle Same as purl, both parameters have same result
+##' @param clean Default TRUE. Remove intermediate LaTeX files when
+##'     using texi2pdf
+##' @param verbose Default = FALSE. Functions try to reduce amount of
+##'     screen output. Knitr functions that use "quiet" flag will be
+##'     set to \code{!verbose}.
 ##' @param envir environment for evaluation, see \code{knitr}
 ##'     documents, defaults to parent.frame().
 ##' @param encoding character encoding, defaults from user options
@@ -279,9 +293,11 @@ rmd2pdf <- function(fn = NULL, wd = NULL, verbose = FALSE, type = "report",
 ##' of1 <- rnw2pdf("skeleton.Rnw", engine = "Sweave")
 ##' list.files()
 ##' setwd(wd.orig)
-rnw2pdf <- function(fn = NULL, wd = NULL, engine = "knitr", verbose = FALSE,
-                    envir = parent.frame(), encoding = getOption("encoding"),
+rnw2pdf <- function(fn = NULL, wd = NULL, engine = "knitr", purl = TRUE,
+                    tangle = purl, clean = TRUE, verbose = FALSE, envir = parent.frame(),
+                    encoding = getOption("encoding"),
                     ...) {
+    if(!missing(tangle) && is.logical(tangle)) purl <- tangle
     if (!is.null(wd)) {
         wd.orig <- getwd()
         setwd(wd)
@@ -291,28 +307,81 @@ rnw2pdf <- function(fn = NULL, wd = NULL, engine = "knitr", verbose = FALSE,
         cat("Will render all *.Rnw files in current working directory\n")
         fn <- list.files(pattern="Rnw")
     }    
-   
+
+    isWindoze <- if(Sys.info()[['sysname']] == "Windows") TRUE else FALSE
+    sysnull <-  if(isWindoze) "> nul" else " > /dev/null"
+    
     compileme <- function(x, verbose) {
         if (length(grep("\\.lyx$", tolower(x)))){
-            if (tolower(engine) == "knitr"){
-                system(paste("lyx -e knitr ", x))
-            } else {
-                system(paste("lyx -e sweave ", x))
-            }
-            system(paste("lyx -e pdf2 ", x))
-            x <- gsub("\\.lyx$", ".Rnw", x)
+            ## Let lyx compile to pdf
+            cmd <- paste("lyx -e pdf2 ", x, if(!verbose) sysnull)
+            if (isWindoze) shell(cmd) else system(cmd)
             fnpdf <- gsub("\\.lyx$", ".pdf", x, ignore.case = TRUE)
+            if(tangle){
+                ## Remove previous R file, avoid confusion
+                unlink(gsub("\\..*$", ".R", x))
+                ## lyx can directly export r code from knitr engine file 
+                if (tolower(engine) == "knitr"){
+                    cmd <- paste("lyx -e r ", x, if(!verbose) sysnull)
+                    if (isWindoze) shell(cmd) else system(cmd)
+                } else {
+                    ## will fail if split=TRUE, so must make copy of file, change to split=FALSE
+                    ## Need to turn split to FALSE in order to Tangle the lyx file.
+                    bak <- "-uniquebackupstring"
+                    fnbackup <- gsub("(.*)(\\..*$)", paste0("\\1", bak, "\\2"), x)
+                    file.copy(x, fnbackup, overwrite = TRUE)
+                    rnwfile <- readLines(fnbackup)
+                    ## Find SweaveOpts line, change split to FALSE
+                    rnwfile[grep("SweaveOpts", rnwfile)] <- gsub("(split\\s*=)\\s*.*,", "\\1FALSE,",
+                                                                 rnwfile[grep("SweaveOpts", rnwfile)])
+                    writeLines(rnwfile, con = fnbackup)
+                    cmd <- paste("lyx -e r", fnbackup, if(!verbose) sysnull)
+                    if (isWindoze) shell(cmd) else system(cmd)
+                    gg <- file.copy(gsub("\\..*$", ".R", fnbackup),
+                                    gsub(bak, "", gsub("\\..*$", ".R", fnbackup)),
+                                    overwrite=TRUE)
+                    if (file.exists(gsub("\\..*$", ".R", x))){
+                        unlink(paste0("*", bak, "*"))
+                    }
+                }
+            }
+            if(verbose){
+                if (tolower(engine) == "knitr"){
+                    cmd <- paste("lyx -e knit ", x, if(!verbose) sysnull)
+                    if (isWindoze) shell(cmd) else system(cmd)
+                } else {
+                    cmd <- paste("lyx -e sweave ", x, if(!verbose) sysnull)
+                    if (isWindoze) shell(cmd) else system(cmd)
+                }
+            }
         } else if (length(grep("\\.rnw$", tolower(x)))) {
             if (tolower(engine) == "knitr"){
-                knitr::knit(x, quiet = !verbose, tangle = TRUE, envir = envir,
+                if(tangle) knitr::knit(x, quiet = !verbose, tangle = tangle, envir = envir,
                             encoding = encoding)
                 fnpdf <- knitr::knit2pdf(x, quiet = !verbose, envir = envir,
                             encoding = encoding)
             } else {
                 Sweave(x)
-                Stangle(x)
+                if (tangle) {
+                    bak <- "-tanglebackupstring"
+                    fnbackup <- gsub("(.*)(\\..*$)", paste0("\\1", bak, "\\2"), x)
+                    file.copy(x, fnbackup, overwrite = TRUE)
+                    rnwfile <- readLines(fnbackup)
+                    rnwfile[grep("SweaveOpts", rnwfile)] <- gsub("(split\\s*=)\\s*.*,", "\\1FALSE,",
+                                                                 rnwfile[grep("SweaveOpts", rnwfile)])
+                    writeLines(rnwfile, con = fnbackup)
+                    Stangle(fnbackup)
+                    fnR <- gsub("\\.Rnw", ".R", fnbackup)
+                    if(file.exists(fnR)){
+                        fnRrename <- file.copy(fnR, gsub(bak, "", fnR))
+                        unlink(paste0("*", bak, "*"))
+                    } else {
+                        warning("rnw2pdf: failed creation of R tangle file")
+                    }
+                    if(file.exists(fnbackup)) unlink(fnbackup)
+                }
                 tools::texi2pdf(gsub("\\.Rnw$", ".tex", x, ignore.case = TRUE),
-                                texi2dvi = "texi2pdf")
+                                texi2dvi = "texi2pdf", clean = clean, quiet = !verbose)
                 fnpdf <- gsub("\\.Rnw$", ".pdf", x, ignore.case = TRUE)
             }
         }
