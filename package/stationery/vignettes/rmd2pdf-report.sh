ecfd320dd003f52411cae6bb4998b8def0dc086d
Paul E. Johnson <pauljohn@ku.edu>
2018-05-04 12:43:35 -0500

    software/crmda/stationery: package update with rnw2pdf updat

--- package/stationery/vignettes/rmd2pdf-report.sh
+++ package/stationery/vignettes/rmd2pdf-report.sh
@@ -3,9 +3,19 @@
 ## Paul Johnson
 ## 2018-02-10
 
-## customization for vignette packaging
-##scriptname=`basename $0 .sh`
-scriptname="rmd2pdf"
+scriptname=`basename $0 .sh`
+
+## Default argument settings
+pwd=`pwd`
+declare -A parms
+parms[engine]=\"Sweave\"
+parms[verbose]=FALSE
+parms[tangle]=TRUE
+## VERBOSE is BASH flag, will cause more script output,
+## and set parms[verbose]=TRUE
+VERBOSE=0
+## DEBUG if for author of this script, for fixing arg parsing. Not for users
+DEBUG=0
 
 ## Receive one filename, then build it.
 compileOne(){
@@ -13,38 +23,39 @@ compileOne(){
 	if [[ -e "$filename" ]]; then
 		fn=$(basename "$filename")
 		exten="${fn##*.}"
-		## check extension, ignore case, allows "rmd" or "RMD"
+		showme "extension is $exten"
+		## check extension, ignore case
 		shopt -s nocasematch
-		if [[ "$exten" == "Rmd" ]]; then
-			echo -e "compile $filename"
-			echo -e "parmstring $parmstring"
-			Rscript -e "library(crmda); $scriptname(\"$filename\", $parmstring); library(knitr); purl(\"$filename\")"
+		if [[ ("$exten" -eq "Rnw") || ( "$exten" -eq "lyx") ]]; then
+			cmd="library(stationery); $scriptname(\"$filename\"$parmstring)"
+			if [[ ${VERBOSE} -gt 0 ]]; then echo -e "Running: $cmd"; fi
+			Rscript -e "${cmd}"
 		else
-			echo -e "Error: $filename. Extension should be \"Rmd\""
+			echo -e "Error: $filename. Extension should be \"Rnw\" or \"lyx\""
 		fi
 	else
  		echo -e "$filename not found"
 	fi
 }
 
-## Scan directory for [Rmd,rmd] files and try to compile them
+## Scan directory for [Rnw] files and try to compile them
 compileall() {
-   	echo -e "\n""If no filename is specified, these Rmd files will be compiled:"
-	echo -e "\n" $(ls -1 *.Rmd) "\n"
+   	echo -e "\n""If no filename is specified, these files will be compiled:"
+	find . -type f \( -iname "*.Rnw" -or -iname "*.lyx" \)
 	echo -e "Hit Enter to continue, or \"q\" to quit"
 	read -p "> " input
 	if [[ $input == "q" ]]; then
    		exit 1
 	else
 		echo "Compiling them all"
-		for fn in *.Rmd
+		for fn in *{.Rnw,.rnw,.lyx} 
 		do
-			compileOne "$fn"
+		 	compileOne "$fn"
 		done
 	fi
 }
 
-
+## Gracefully quit with error message
 die() {
 	printf '%s\n' "$1" >&2
 	exit 1
@@ -77,38 +88,16 @@ catarr() {
 
 ## Usage instruction.  
 usage() {
-	echo -e "\nUsage: $0 --arg=value [filename.Rmd]".
-	echo -e "Note: Because this document uses a template, the yaml header"
-	echo -e "format settings are ignored. Instead, specify arguments for this script."
+	echo -e "\nUsage: $0 --arg=value <filename.Rnw>".
 	echo "Current arguments:"
 	printarr parms
-    echo -e "\nThis script reformats and sends request to R as:\n"
-	echo -e "library(crmda); $scriptname(\"filename.Rmd\""$parmstring")\n"
+    echo -e "\nThis script reformats and sends request to R:\n"
+	echo -e "library(stationery); $scriptname(\"filename.Rnw\""$parmstring")\n"
     echo "Add argument -v for VERBOSE output."
 	echo "Any arguments described in documentation for $scriptname R function are allowed."
-	echo "CAUTION"
-	echo "Arguments that are quoted strings, such as --type=\"report\""
-    echo "or --template=\"report-boilerplate.tex\" need special care when enterd from command line." 
-    echo "It is necessary to 'protect' (escape) the quotation marks." 
-    echo "We suggest command line usage that wraps them inside single quotes, such as:"
-	echo "--type='\"report\"' --template='\"report-boilerplate.tex\"'"
 }
 
-## VERBOSE is flag users can turn on to get more detailed output
-VERBOSE=0
-## DEBUG if for author of this script, for fixing arg parsing. Not for users
-DEBUG=0
-pwd=`pwd`
-declare -A parms
-parms[toc]=TRUE
-parms[toc_depth]=2
-parms[clean]=TRUE
-parms[quiet]=TRUE
-parms[verbose]=FALSE
-parms[keep_md]=FALSE
-parms[type]=\"report\"
-parms[template]=\"theme/report-boilerplate.tex\"
-parms[output_dir]=\"$pwd\"
+
 
 optspec=":vh-:"
 while getopts "$optspec" OPTCHAR; do
@@ -140,7 +129,8 @@ while getopts "$optspec" OPTCHAR; do
 			## if -v flag is present
 			echo -e "\n\nverbose flag\n"
             VERBOSE=1
-			parms["quiet"]="FALSE"
+			parms["quiet"]=FALSE
+			parms["verbose"]=TRUE
 			## must print default parms here, before more parsing
 			echo "Parameters are:" >&2
 			printarr parms
@@ -155,6 +145,7 @@ done
 
 parmstring=""
 catarr parms
+## echo "parmstring: $parmstring"
 
 ## No shifts inside there, so must throw away arguments
 ## that were processed
