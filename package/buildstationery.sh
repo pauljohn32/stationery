3bed4d0308feb677d4bdba511974ca1bdd0c4bc7
Paul E. Johnson <pauljohn@ku.edu>
2018-08-15 15:36:01 -0500

    1. Delete the tricks used to hide from R vignette builder checks, including .index_extras and build/vignette.rds. No longer manually keep index.html, let vignette builder do that.

--- package/buildstationery.sh
+++ package/buildstationery.sh
@@ -8,31 +8,38 @@ mkdir ${PACKAGE}.gitex
 
 cd ${PACKAGE}
 
-cd vignettes
-./compiler.sh
-cd ..
+## cd vignettes
+## ./compiler.sh
+## cd ..
 
 ## copies UNCOMMITTED but TRACKED files.
 git ls-files . | tar cT - | tar -x -C "../${PACKAGE}.gitex"
 cd ..
 
-cd ${PACKAGE}.gitex
-mkdir -p inst/doc
-cp -f ../stationery/vignettes/*.pdf* inst/doc
-cp -f ../stationery/vignettes/*.html* inst/doc
-
-cd ..
+## cd ${PACKAGE}.gitex
+## mkdir -p inst/doc
+## mv -f ../stationery/vignettes/*.pdf* inst/doc
+## mv -f ../stationery/vignettes/*.html* inst/doc
+## touch inst/doc/index.html
+## cd ..
 R --vanilla -f roxygenstationery.R
 
 
-R CMD build --resave-data --no-build-vignettes ${PACKAGE}.gitex 
-## R CMD build --resave-data ${PACKAGE}.gitex 
+## R CMD build --resave-data --no-build-vignettes ${PACKAGE}.gitex 
+## R CMD build --no-resave-data --compact-vignettes="both" ${PACKAGE}.gitex 
+
+## /usr/lib/R/bin/R --no-site-file --no-environ --no-save --no-restore --quiet  \
+##                 CMD build 'stationery.gitex'  --no-resave-data --no-manual \
+##                 --compact-vignettes="both" --no-build-vignettes
+
+/usr/lib/R/bin/R --no-site-file --no-environ --no-save --no-restore --quiet  \
+                 CMD build 'stationery.gitex'  --no-resave-data --no-manual \
+                 --compact-vignettes="both"
 
 read -p "Run check: OK? (y or n)" result
 
 if [ $result = "y" ];  then
-R CMD check --as-cran --no-build-vignettes ${PACKAGE}_${VERSION}.tar.gz
-## R CMD check --as-cran ${PACKAGE}_${VERSION}.tar.gz
+R CMD check --as-cran ${PACKAGE}_${VERSION}.tar.gz
 fi
 
 read -p "Install: OK? (y or n)" result
