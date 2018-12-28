37b2175c08995f53b871a102d054b7cf917fde4b
Paul E. Johnson <pauljohn@ku.edu>
2018-12-27 18:11:04 -0600

    getFiles: unlink the example directory

--- package/stationery/R/utils.R
+++ package/stationery/R/utils.R
@@ -44,6 +44,7 @@
 ##' getFiles("logo.pdf", pkg = "stationery", overwrite = TRUE, outputdir = file.path(tdir, "theme"),
 ##'          backup = TRUE)
 ##' list.files(file.path(tdir, "theme"))
+##' unlink(tdir, recursive = TRUE)
 getFiles <- function(fn, pkg = "stationery",
                      overwrite = FALSE, backup = overwrite,
                      pkgdir = "theme", outputdir = "theme")
