260525b286890f24f858aac745d31c721a78562b
Paul E. Johnson <pauljohn@ku.edu>
2019-02-13 14:33:58 -0600

    Test version bump: 0.98.5.8

--- package/stationery/inst/rmarkdown/templates/compileall.sh
+++ package/stationery/inst/rmarkdown/templates/compileall.sh
@@ -25,6 +25,37 @@ for i in rn*sweave; do
     cd ../../
 done
 
+
+
+for i in rmd2html*; do
+    echo $i
+    cd $i/skeleton
+    echo $i
+    for j in *Rmd; do
+       ./rmd2html.sh  $j
+    done
+    rm -f Rplots.pdf
+    rm -f *.Rout
+    ls -la
+    cd ../../
+done
+
+
+
+for i in rmd2pdf*; do
+    echo $i
+    cd $i/skeleton
+    echo $i
+    for j in *Rmd; do
+       ./rmd2pdf.sh  $j
+    done
+    rm -f Rplots.pdf
+    rm -f *.Rout
+    ls -la
+    cd ../../
+done
+
+
 # this for cleaning up Latex compiler files too
 # for i in rn*sweave; do
 #     echo $i
